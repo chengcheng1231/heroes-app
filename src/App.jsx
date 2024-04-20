fd3a1f49b4dc6ce669784a37fb576097a12635d5
yucheng <yuchenghsu@gmail.com>
2024-04-20 15:12:30 +0800

    chore(root): change app file format
    use typescript in this project

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+import './App.css';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #bf4f74;
+`;
+
+// Create a Wrapper component that'll render a <section> tag with some styles
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      <Wrapper>
+        <Title>Hello World, this is my first styled component!</Title>
+      </Wrapper>
+    </div>
+  );
+}
+
+export default App;
