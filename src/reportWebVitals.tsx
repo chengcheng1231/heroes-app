84186cf3721475509e7e8e5a87ab848a08380c33
yucheng <yuchenghsu@gmail.com>
2024-04-20 21:40:46 +0800

    chore(root & src): set typescript json file

--- src/reportWebVitals.tsx
+++ src/reportWebVitals.tsx
@@ -1,4 +1,8 @@
-const reportWebVitals = onPerfEntry => {
+import * as webVitals from 'web-vitals';
+
+type OnPerfEntry = Parameters<typeof webVitals.getCLS>[0];
+
+const reportWebVitals = (onPerfEntry?: OnPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
